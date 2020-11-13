7a1a47a99f36c47c1ef5775dd9c5af19a28be5df
FurkanMera <74408149+FurkanMera@users.noreply.github.com>
2020-11-13 14:46:21 +0300

    Update file.lua

--- source/file.lua
+++ source/file.lua
@@ -1,34 +1,24 @@
 local value = {}
-local config = gg.EXT_CACHE_DIR.. "/"..
+local config = gg.EXT_CACHE_DIR.."/"..
 gg.getFile():match("").. ".cfg"
 local data = loadfile(config)
-if data ~= nil then
-value = data() data = nil end
-
-id = "nickname"
-pass = "pass"
-while(true)do
-value = gg.prompt({"Kullanici Adi","Sifre"},value,{"text","text"})
-if value == nil then
-gg.toast("Hic bir sey secmedin")
+if data ~= nil then value = data() data = nil end id = "nickname"pass = "pass"while(true)do value = gg.prompt({"Kullanici Adi","Sifre"},value,{"text","text"})
+if value == nil 
+then gg.toast("Hic bir sey secmedin")
 return
-end
-
-if value[3] then
-gg.saveVariable(value, config)
+end 
+if value[3]
+ then gg.saveVariable(value, config)os.remove(config)
 else
-os.remove(config)
 end
 if id == value[1] and pass == value[2] then gg.alert("Giris Basarili")
 break
-else if
- id ~= value[1] and pass == value[2] then gg.alert("Nickname hatalı")
-else if
- id == value[1] and pass ~= value[2] then gg.alert("Pass hatalı")
-else
+else 
+if id ~= value[1] and pass == value[2] then gg.alert("Nickname hatalı")
+else if 
+id == value[1] and pass ~= value[2] then gg.alert("Pass hatalı")
+else 
 gg.alert("Ikiside hatali")
-end end end end
-
-
+end end end end 
 
 choice = gg.choice({"Hack/hile","Exit/cikis"})
